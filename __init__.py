f4499b5e3c23d2855b14f0186cbfbc813f6bd554
Julian Selley <j.selley@manchester.ac.uk>
2011-12-16 16:20:20 +0000

    - initial commit of files -- __init__.py relates to the proteomics package -- epydoc.conf relates to the documentation of the package; in particular to the configuration of the epydoc software

---
+++ __init__.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) University of Manchester 2011 
+#               Julian Selley <j.selley@manchester.ac.uk>
+################################################################################
+
+"""Proteomics module
+
+This module provides methodologies linked with Proteomics and Protein Mass
+Spectrometry. It actually loads in a load of other modules.
+
+Overview
+========
+
+TODO 201111180955 JNS: write the documentation for this library
+
+"""
+
+# Metadata
+__version__   = '0.01'
+__author__    = 'Julian Selley <j.selley@manchester.ac.uk>'
+__copyright__ = 'Copyright 2011 Julian Selley <j.selley@manchester.ac.uk>'
+__license__   = '''\
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+'''
+
+__all__ = [ "mascot" ]
