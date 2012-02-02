286d42eb1e829348dde16ac2c94aea8edbd3ed68
Julian Selley <j.selley@manchester.ac.uk>
2012-02-02 10:33:14 +0000

    - change of version number (using standard format now of   major.minor.patch.subpatch). Currently, although the git tag is   v1.0, I have left this at version 0.1 until I have had chance to   check and test the install procedure - changed the license to The Artistic License 2.0, to tie in with other   code the FLS Bioinformatics Core is producing. After discussions about   the different licenses and the pro's and con's, we felt that the GPL   scares a lot of people, and ultimately doesn't really matter in the finner   detail to us, but that The Artistic License allows developers to detail   any changes they make to the code to the authors.

--- test_mascot.py
+++ test_mascot.py
@@ -22,19 +22,10 @@ TODO 201112130955 JNS: write the documentation for this test suite
 """
 
 # Metadata
-__version__   = '0.01'
+__version__   = '0.1.0'
 __author__    = 'Julian Selley <j.selley@manchester.ac.uk'
 __copyright__ = 'Copyright 2011 Julian Selley <j.selley@manchester.ac.uk>'
-__license__   = '''\
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-'''
+__license__   = 'The Artistic License 2.0 (see the file LICENSE included with the distribution)'
 
 # Imports
 import mascot
